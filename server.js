340a83f6b2fb1ba6f7b643daec1db836d88809b7
Marat Miribyan <122740363+Miribyan@users.noreply.github.com>
2023-04-03 23:38:20 +0400

    last fixes

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const port = 4000;
+app.use(express.static("./build"));
+app.listen(port, () => {
+  console.log(`Listening on port ${port}…`);
+  start();
+});
