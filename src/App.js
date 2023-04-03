94b6383119bbe5b23085348eef4a13aa982d3696
Marat Miribyan <122740363+Miribyan@users.noreply.github.com>
2023-04-04 00:12:39 +0400

    fix locale

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ function App() {
         <select
           id="location"
           onChange={handleLangChange}
-          value={localStorage.getItem("i18nextLng")}
+          value={localStorage.getItem("i18nextLng")?? "en"}
           name="location"
           className="mt-2 block absolute top-0 mb-2 right-8 rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
         >
